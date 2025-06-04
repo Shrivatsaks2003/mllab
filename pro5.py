cb96f4bc1c01ed2cd49110d9ed0cc048e8d027f8
Shrivatsa K S <ramamanis14@gmail.com>
2025-06-04 23:26:29 +0530

    five

--- pro5.py
+++ pro5.py
@@ -1,12 +1,13 @@
-import pyforest
+import numpy as np, matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 
 X = np.random.rand(100, 1)
-y = np.where(X[:50] <= 0.5, 1, 2)
+y = np.where(X[:50] <= 0.5, 1, 2)  # 1D array, no warning
 
 plt.figure(figsize=(10, 6))
 for i, k in enumerate([1, 2, 3, 4, 5, 20, 30]):
     model = KNeighborsClassifier(n_neighbors=k)
-    model.fit(X[:50], y.ravel())
+    model.fit(X[:50], y.ravel())  # Use ravel() here
     pred = model.predict(X[50:])
     
     plt.subplot(3, 3, i + 1)
