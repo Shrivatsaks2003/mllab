ed9b75890b5bfa8abef59d825e67708efb4a5d1b
Shrivatsa K S <ramamanis14@gmail.com>
2025-06-05 01:33:58 +0530

    its changed

--- pro7.py
+++ pro7.py
@@ -1,25 +1,22 @@
 import pyforest
 
-def linear_regression_california():
-    X, y = fetch_california_housing(as_frame=True).data[["AveRooms"]], fetch_california_housing().target
+def linear_regression_boston():
+    df = fetch_openml("boston", version=1, as_frame=True).frame
+    X, y = df[["RM"]], df["MEDV"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     model = LinearRegression().fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    plt.scatter(X_test, y_test); plt.plot(X_test, y_pred, color='red'); plt.show()
-    print("MSE:", mean_squared_error(y_test, y_pred), "R2:", r2_score(y_test, y_pred))
+    plt.scatter(X_test, y_test); plt.plot(X_test, y_pred, color='red'); plt.title("Boston Housing"); plt.show()
+    print("Boston | MSE:", mean_squared_error(y_test, y_pred), "| R²:", r2_score(y_test, y_pred))
 
 def polynomial_regression_auto_mpg():
-    url = "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data"
-    cols = ["mpg", "cyl", "disp", "hp", "wt", "acc", "yr", "org"]
-    df = pd.read_csv(url, sep=r'\s+', names=cols, na_values="?").dropna()
-    X, y = df[["disp"]], df["mpg"]
+    df = sns.load_dataset("mpg").dropna()
+    X, y = df[["displacement"]], df["mpg"]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     model = make_pipeline(PolynomialFeatures(2), StandardScaler(), LinearRegression()).fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    plt.scatter(X_test, y_test); plt.scatter(X_test, y_pred, color='red'); plt.show()
-    print("MSE:", mean_squared_error(y_test, y_pred), "R2:", r2_score(y_test, y_pred))
+    plt.scatter(X_test, y_test); plt.scatter(X_test, y_pred, color='red'); plt.title("Auto MPG"); plt.show()
+    print("Auto MPG | MSE:", mean_squared_error(y_test, y_pred), "| R²:", r2_score(y_test, y_pred))
 
-linear_regression_california()
+linear_regression_boston()
 polynomial_regression_auto_mpg()
-
-# code eddited
